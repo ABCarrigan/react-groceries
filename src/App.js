d2c487c0577b880ab44709545527d9d4352f2d8a
ABCarrigan <addisonbcarrigan@gmail.com>
2022-03-27 23:56:42 -0400

    div creation working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import './App.css'
+import GroceryParams from './services/groceryApi'
 
 function App() {
+  const [groceries, setGroceries] = useState(GroceryParams)
+  const createGroceryDiv = () => {
+      const newGrocery = {
+        item: (document.getElementById('item').value),
+        brand: (document.getElementById('brand').value),
+        pounds: (document.getElementById('pounds').value),
+        quantity: (document.getElementById('quantity').value),
+        isPurchased: (false)
+    }
+      setGroceries(groceries.concat([{
+        newGrocery
+      }]))
+  }
+  useEffect(() => {
+      document.getElementById('submit').addEventListener('click', createGroceryDiv)}, groceries)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="bodyContainer">
+      <h3>React Groceries</h3>
+      <div className="groceryContainer">
+                {groceries.map((each,i) => {
+                    return(
+                        <div className="groceryItem">
+                            Item name: {each.item}<br/>
+                            Brand: {each.brand}<br/>
+                            Weight:{each.pounds} lbs<br/>
+                            Quantity: {each.quantity}<br/>
+                            Is purchased?: <input type="checkbox" name="isPurchased" value={each.isPurchased} /><br/>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="groceryCreateButton">
+                <form>
+                    Item Name: <input type='text' id='item' />
+                    <br></br>
+                    Brand: <input type='text' id='brand' />
+                    <br></br>
+                    Pounds: <input type="number" id="pounds" />
+                    <br></br>
+                    Quantity: <input type="number" id="quantity" />
+                    <br></br>
+                    <input type='button' id="submit" value='Create grocery!'/>
+                </form>
+            </div>
+   </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/services/groceryApi.js
@@ -0,0 +1,22 @@
+const GroceryParams = [
+    {
+    item: 'Apples',
+    brand: 'Gala',
+    pounds: (0.5),
+    quantity: (2),
+    isPurchased: false
+  }, {
+    item: 'Bananas',
+    brand: 'Chiquita',
+    pounds: (1.0),
+    quantity: (6),
+    isPurchased: false
+  }, {
+    item: 'Oranges',
+    brand: 'Sunkist',
+    pounds: (1.5),
+    quantity: (10),
+    isPurchased: false
+  }
+]
+export default GroceryParams
